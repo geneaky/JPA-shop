ae5d7ce186d535dc0f4be23f6b5788d52ec75093
geneaky <kaktus418@gmail.com>
2021-03-09 00:45:49 +0900

    item service

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -6,13 +6,13 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.support.ManagedMap;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
 class MemberServiceTest {
@@ -38,6 +38,18 @@ class MemberServiceTest {
 
     @Test
     public void 중복_회원_예외() throws Exception{
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
 
+        Member member2 = new Member();
+        member2.setName("kim");
+        //when
+        memberService.join(member1);
+
+        //then
+        assertThrows(IllegalStateException.class,()->{
+            memberService.join(member2);
+        });
     }
 }
